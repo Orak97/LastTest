a453f6a1385afb342f41c859052e8d5782d7e9be
Orak97 <50458698+Orak97@users.noreply.github.com>
2022-02-16 18:47:55 +0100

    Addedd first code

--- LastTest/src/main/java/logic/HelloWorld.java
+++ LastTest/src/main/java/logic/HelloWorld.java
@@ -2,6 +2,6 @@ package logic;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Sono partito");;
+		System.out.println("Sono partito e mi sono fatto molto male!");;
 	}
 }
